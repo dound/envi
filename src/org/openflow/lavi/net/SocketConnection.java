3c8bf7fe8e7421774db96e2e77e63a2082b5f96e
David Underhill <dgu@cs.stanford.edu>
2009-01-10 10:48:17 -0800

    remove unneeded import

--- src/org/openflow/lavi/net/SocketConnection.java
+++ src/org/openflow/lavi/net/SocketConnection.java
@@ -1,7 +1,6 @@
 package org.openflow.lavi.net;
 
 import java.io.DataInput;
-import java.io.DataInputStream;
 import java.io.DataOutput;
 import java.io.DataOutputStream;
 import java.io.IOException;

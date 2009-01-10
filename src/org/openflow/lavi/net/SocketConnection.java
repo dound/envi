fb92c396f5f32b36e00802499133b8b7a70128e5
David Underhill <dgu@cs.stanford.edu>
2009-01-09 19:44:50 -0800

    moved SocketConnection from org.openflow.lavi.net.util to org.openflow.lavi.net

---
+++ src/org/openflow/lavi/net/SocketConnection.java
@@ -0,0 +1,97 @@
+package org.openflow.lavi.net.util;
+
+import java.io.DataInput;
+import java.io.DataInputStream;
+import java.io.DataOutput;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.net.Socket;
+
+/**
+ * Provides helper functions for setting up and sending and receiving binary 
+ * and ASCII data over a TCP connection.
+ * 
+ * @author David Underihll
+ */
+public class SocketConnection implements DataInput, DataOutput {
+    /** the socket which connects us to the NOX server */
+    public final Socket s;
+
+    /** output stream to write to the socket */
+    public final DataOutputStream out;
+
+    /** input stream to read from the socket */
+    public final DataInputStream in;
+    
+    /** 
+     * Connect to the client on the specified port.
+     * 
+     * @param ip    the IP to connect to
+     * @param port  the TCP port to connect on
+     */
+    public SocketConnection(String ip, int port) {
+        // setup socket for listening for new clients connection requests
+        Socket stmp;
+        try {
+            stmp = new Socket(ip, port);
+        }
+        catch(IOException e) {
+            System.err.println(Integer.toString(port) + ": " + e.getMessage());
+            s = null;
+            out = null;
+            in = null;
+            return;
+        }
+        s = stmp;
+        
+        //try to establish the I/O streams: if we can't establish either, then close the socket
+        DataOutputStream tmp;
+        try {
+            tmp = new DataOutputStream(s.getOutputStream());
+        } catch(IOException e) {
+            System.err.println("Client Socket Setup Error: " + e.getMessage());
+            tmp = null;
+        } 
+        out = tmp;
+
+        DataInputStream tmp2;
+        try {
+            tmp2 = new DataInputStream(s.getInputStream());
+        } catch(IOException e) {
+            System.err.println("Client Socket Setup Error: " + e.getMessage());
+            tmp2 = null;
+        }
+        in = tmp2;
+    }
+
+    public void readFully(byte b[])                   throws IOException { in.readFully(b); }
+    public void readFully(byte b[], int off, int len) throws IOException { in.readFully(b, off, len); }
+    public int skipBytes(int n)                       throws IOException { return in.skipBytes(n); }
+    public boolean readBoolean()                      throws IOException { return in.readBoolean(); }
+    public byte readByte()                            throws IOException { return in.readByte(); }
+    public int readUnsignedByte()                     throws IOException { return in.readUnsignedByte(); }
+    public short readShort()                          throws IOException { return in.readShort(); }
+    public int readUnsignedShort()                    throws IOException { return in.readUnsignedShort(); }
+    public char readChar()                            throws IOException { return in.readChar(); }
+    public int readInt()                              throws IOException { return in.readInt(); }
+    public long readLong()                            throws IOException { return in.readLong(); }
+    public float readFloat()                          throws IOException { return in.readFloat(); }
+    public double readDouble()                        throws IOException { return in.readDouble(); }
+    public String readLine()                          throws IOException { return in.readLine(); }
+    public String readUTF()                           throws IOException { return in.readUTF(); }
+
+    public void write(int b)                          throws IOException { out.write(b); }
+    public void write(byte b[])                       throws IOException { out.write(b); }
+    public void write(byte b[], int off, int len)     throws IOException { out.write(b,off,len); }
+    public void writeBoolean(boolean v)               throws IOException { out.writeBoolean(v); }
+    public void writeByte(int v)                      throws IOException { out.writeByte(v); }
+    public void writeShort(int v)                     throws IOException { out.writeShort(v); }
+    public void writeChar(int v)                      throws IOException { out.writeChar(v); }
+    public void writeInt(int v)                       throws IOException { out.writeInt(v); }
+    public void writeLong(long v)                     throws IOException { out.writeLong(v); }
+    public void writeFloat(float v)                   throws IOException { out.writeFloat(v); }
+    public void writeDouble(double v)                 throws IOException { out.writeDouble(v); }
+    public void writeBytes(String s)                  throws IOException { out.writeBytes(s); }
+    public void writeChars(String s)                  throws IOException { out.writeChars(s); }
+    public void writeUTF(String s)                    throws IOException { out.writeUTF(s); }
+}

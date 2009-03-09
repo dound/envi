f39a08fdcb892f48f50897f34318c39dafc8e863
David Underhill <dgu@cs.stanford.edu>
2009-03-09 00:53:18 -0700

    icons can now be used as a Drawable with the DrawableIcon wrapper

---
+++ gui/src/org/openflow/lavi/drawables/DrawableIcon.java
@@ -0,0 +1,32 @@
+package org.openflow.lavi.drawables;
+
+import java.awt.Graphics2D;
+
+import org.pzgui.AbstractDrawable;
+import org.pzgui.icon.Icon;
+
+public class DrawableIcon extends AbstractDrawable {
+    private Icon icon;
+    private int x, y;
+    private int w, h;
+    
+    public DrawableIcon(Icon icon, int x, int y) {
+        this(icon, x, y, icon.getWidth(), icon.getHeight());
+    }
+
+    public DrawableIcon(Icon icon, int x, int y, int w, int h) {
+        this.icon = icon;
+        this.x = x;
+        this.y = y;
+        this.w = w;
+        this.h = h;
+    }
+    
+    public void drawObject(Graphics2D gfx) {
+        icon.draw(gfx, x, y, w, h);
+    }
+    
+    public boolean isWithin(int x, int y) {
+        return icon.contains(x, y);
+    }
+}

f94ae42d03b2653386745963eb1ae71ad6c7baf4
David Underhill <dgu@cs.stanford.edu>
2009-03-09 16:57:09 -0700

    make xid a default arg

--- backend/protocol/LAVIMessage.py
+++ backend/protocol/LAVIMessage.py
@@ -24,7 +24,7 @@ def dpidstr(ll):
 class LAVIMessage(LTMessage):
     SIZE = 4
 
-    def __init__(self, xid):
+    def __init__(self, xid=0):
         LTMessage.__init__(self)
         self.xid = xid
 
@@ -46,7 +46,7 @@ class Disconnect(LAVIMessage):
     def get_type():
         return 0x00
 
-    def __init__(self, xid):
+    def __init__(self, xid=0):
         LAVIMessage.__init__(self, xid)
 
     def __str__(self):
@@ -57,7 +57,7 @@ class PollStart(LAVIMessage):
     def get_type():
         return 0x0E
 
-    def __init__(self, xid, interval_in_100ms_units, lm):
+    def __init__(self, interval_in_100ms_units, lm, xid=0):
         LAVIMessage.__init__(self, xid)
         self.interval = interval_in_100ms_units
         self.lm = lm
@@ -81,7 +81,7 @@ class PollStart(LAVIMessage):
         body = body[1:]
         lm = LAVI_PROTOCOL.unpck_received_msg(type_val, body)
 
-        return PollStart(xid, interval, lm)
+        return PollStart(interval, lm, xid)
 
     def __str__(self):
         fmt = 'POLL_START: ' + LAVIMessage.__str__(self) + ' interval=%s msg=%s'
@@ -92,7 +92,7 @@ class PollStop(LAVIMessage):
     def get_type():
         return 0x0F
 
-    def __init__(self, xid, xid_to_stop_polling):
+    def __init__(self, xid_to_stop_polling, xid=0):
         LAVIMessage.__init__(self, xid)
         self.xid_to_stop_polling = xid_to_stop_polling
 
@@ -107,7 +107,7 @@ class PollStop(LAVIMessage):
         xid = struct.unpack('> I', body[:4])[0]
         body = body[4:]
         xid_to_stop_polling = struct.unpack('> I', body[:4])[0]
-        return PollStop(xid, xid_to_stop_polling)
+        return PollStop(xid_to_stop_polling, xid)
 
     def __str__(self):
         return 'POLL_STOP: ' + LAVIMessage.__str__(self) + ' xid_to_stop_polling=' + self.xid_to_stop_polling
@@ -117,14 +117,17 @@ class SwitchesRequest(LAVIMessage):
     def get_type():
         return 0x10
 
-    def __init__(self, xid):
-        LAVIMessage.__init__(self, xid)
+    def __init__(self, k):
+        LAVIMessage.__init__(self, k)
+
+    def getK(self):
+        return self.xid
 
     def __str__(self):
-        return 'SWITCHES_REQUEST: ' + LAVIMessage.__str__(self)
+        return 'SWITCHES_REQUEST: k=%u' % self.getK()
 
 class SwitchesList(LAVIMessage):
-    def __init__(self, xid, dpids):
+    def __init__(self, dpids, xid=0):
         LAVIMessage.__init__(self, xid)
         self.dpids = dpids
 
@@ -141,7 +144,7 @@ class SwitchesList(LAVIMessage):
         num_dpids = len(body) / 8
         fmt = '> %uQ' % num_dpids
         dpids = [dpid for dpid in struct.unpack(fmt, body)]
-        return SwitchesList(xid, dpids)
+        return SwitchesList(dpids, xid)
 
     def __str__(self):
         return LAVIMessage.__str__(self) + ' dpids=[%s]' % ''.join([dpidstr(dpid) + ',' for dpid in self.dpids])
@@ -151,8 +154,8 @@ class SwitchesAdd(SwitchesList):
     def get_type():
         return 0x11
 
-    def __init__(self, xid, dpids):
-        SwitchesList.__init__(self, xid, dpids)
+    def __init__(self, dpids, xid=0):
+        SwitchesList.__init__(self, dpids, xid)
 
     def __str__(self):
         return 'SWITCHES_ADD: ' + SwitchesList.__str__(self)
@@ -162,8 +165,8 @@ class SwitchesDel(SwitchesList):
     def get_type():
         return 0x12
 
-    def __init__(self, xid, dpids):
-        SwitchesList.__init__(self, xid, dpids)
+    def __init__(self, dpids, xid=0):
+        SwitchesList.__init__(self, dpids, xid)
 
     def __str__(self):
         return 'SWITCHES_DEL: ' + SwitchesList.__str__(self)
@@ -173,7 +176,7 @@ class LinksRequest(LAVIMessage):
     def get_type():
         return 0x13
 
-    def __init__(self, xid, src_dpid):
+    def __init__(self, src_dpid, xid=0):
         LAVIMessage.__init__(self, xid)
         self.src_dpid = src_dpid
 
@@ -188,7 +191,7 @@ class LinksRequest(LAVIMessage):
         xid = struct.unpack('> I', body[:4])[0]
         body = body[4:]
         src_dpid = struct.unpack('> Q', body[:8])[0]
-        return LinksRequest(xid, src_dpid)
+        return LinksRequest(src_dpid, xid)
 
     def __str__(self):
         return 'LINKS_REQUEST: ' + LAVIMessage.__str__(self) + ' src_dpid=' + dpidstr(self.src_dpid)
@@ -211,7 +214,7 @@ class Link:
         return Link(src_dpid, t[1], t[2], t[3])
 
 class LinksList(LAVIMessage):
-    def __init__(self, xid, links):
+    def __init__(self, links, xid=0):
         LAVIMessage.__init__(self, xid)
         self.links = links
 
@@ -239,7 +242,7 @@ class LinksList(LAVIMessage):
         for _ in range(num_links):
             links.append(Link.unpack(src_dpid, body[:12]))
             body = body[12:]
-        return LinksList(xid, links)
+        return LinksList(links, xid)
 
     def __str__(self):
         return LAVIMessage.__str__(self) + ' links=%s' % str(self.links)
@@ -249,8 +252,8 @@ class LinksAdd(LinksList):
     def get_type():
         return 0x14
 
-    def __init__(self, xid, links):
-        LinksList.__init__(self, xid, links)
+    def __init__(self, links, xid=0):
+        LinksList.__init__(self, links, xid)
 
     def __str__(self):
         return 'LINKS_ADD: ' + LinksList.__str__(self)
@@ -260,14 +263,14 @@ class LinksDel(LinksList):
     def get_type():
         return 0x15
 
-    def __init__(self, xid, links):
-        LinksList.__init__(self, xid, links)
+    def __init__(self, links, xid=0):
+        LinksList.__init__(self, links, xid)
 
     def __str__(self):
         return 'LINKS_DEL: ' + LinksList.__str__(self)
 
 class Subscribe(LAVIMessage):
-    def __init__(self, xid, subscribe):
+    def __init__(self, subscribe, xid=0):
         LAVIMessage.__init__(self, xid)
         self.subscribe = subscribe
 
@@ -282,7 +285,7 @@ class Subscribe(LAVIMessage):
         xid = struct.unpack('> I', body[:4])[0]
         body = body[4:]
         subscribe = struct.unpack('> B', body[:1])[0]
-        return (xid, True if subscribe==1 else False)
+        return (True if subscribe==1 else False, xid)
 
     def __str__(self):
         what = ' ' if self.subscribe else ' un'
@@ -309,8 +312,8 @@ class LinksSubscribe(Subscribe):
     def get_type():
         return 0x17
 
-    def __init__(self, xid, subscribe):
-        Subscribe.__init__(self, xid, subscribe)
+    def __init__(self, subscribe, xid=0):
+        Subscribe.__init__(self, subscribe, xid)
 
     @staticmethod
     def unpack(body):
@@ -325,7 +328,7 @@ class ETTrafficMatrix(LAVIMessage):
     def get_type():
         return 0xF0
 
-    def __init__(self, xid, k, demand, edge, agg, plen):
+    def __init__(self, k, demand, edge, agg, plen, xid=0):
         LAVIMessage.__init__(self, xid)
         self.k      = int(k)
         self.demand = int(demand)
@@ -344,7 +347,7 @@ class ETTrafficMatrix(LAVIMessage):
         xid = struct.unpack('> I', body[:4])[0]
         body = body[4:]
         t = struct.unpack('> 5I', body[:20])
-        return ETTrafficMatrix(xid, t[0], t[1], t[2], t[3], t[4])
+        return ETTrafficMatrix(t[0], t[1], t[2], t[3], t[4], xid)
 
     def __str__(self):
         fmt = 'ET_TRAFFIC_MATRIX: ' + LAVIMessage.__str__(self) + " k=%u demand=%u edge=%u agg=%u plen=%u"
@@ -370,7 +373,7 @@ class ETLinkUtils(LAVIMessage):
     def get_type():
         return 0xF1
 
-    def __init__(self, xid, utils):
+    def __init__(self, utils, xid=0):
         LAVIMessage.__init__(self, xid)
         self.utils = utils
 
@@ -390,7 +393,7 @@ class ETLinkUtils(LAVIMessage):
         for _ in range(num_utils):
             utils.append(Link.unpack(body[:ETLinkUtil.SIZE]))
             body = body[ETLinkUtil.SIZE:]
-        return ETLinkUtils(xid, utils)
+        return ETLinkUtils(utils, xid)
 
     def __str__(self):
         return 'ET_LINK_UTILS: ' + LAVIMessage.__str__(self) + ' utils=%s' % str(self.utils)
@@ -400,7 +403,7 @@ class ETPowerUsage(LAVIMessage):
     def get_type():
         return 0xF2
 
-    def __init__(self, xid, watts_current, watts_traditional, watts_max):
+    def __init__(self, watts_current, watts_traditional, watts_max, xid=0):
         LAVIMessage.__init__(self, xid)
         self.watts_current = int(watts_current)
         self.watts_traditional = int(watts_traditional)
@@ -422,7 +425,7 @@ class ETPowerUsage(LAVIMessage):
         watts_traditional = struct.unpack('> I', body[:4])[0]
         body = body[4:]
         watts_max = struct.unpack('> I', body[:4])[0]
-        return ETPowerUsage(xid, watts_current, watts_traditional, watts_max)
+        return ETPowerUsage(watts_current, watts_traditional, watts_max, xid)
 
     def __str__(self):
         fmt = 'ET_POWER_USAGE: ' + LAVIMessage.__str__(self) + " cur=%u trad=%u max=%u"
@@ -433,8 +436,8 @@ class ETSwitchesOff(SwitchesList):
     def get_type():
         return 0xF3
 
-    def __init__(self, xid, dpids):
-        SwitchesList.__init__(self, xid, dpids)
+    def __init__(self, dpids, xid=0):
+        SwitchesList.__init__(self, dpids, xid)
 
     def __str__(self):
         return 'SWITCHES_OFF: ' + SwitchesList.__str__(self)
@@ -478,7 +481,7 @@ if __name__ == "__main__":
     def callback():
         if len(server.connections) > 0:
             print 'sending ...'
-            server.send(SwitchesAdd(0, [v+1 for v in range(99)]))
+            server.send(SwitchesAdd([v+1 for v in range(99)]))
         else:
             reactor.callLater(1, callback)
     reactor.callLater(1, callback)

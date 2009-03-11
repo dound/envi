3c5c6617123c9c6b70f16f877d4870f695678ce3
David Underhill <dgu@cs.stanford.edu>
2009-03-11 00:03:36 -0700

    overhaul layout to use automatic GroupLayout; moved sidebar to the bottom

--- gui/src/org/pzgui/layout/ElasticTreeManager.java
+++ gui/src/org/pzgui/layout/ElasticTreeManager.java
@@ -31,7 +31,7 @@ public class ElasticTreeManager extends PZLayoutManager {
     public static final int LBL_WIDTH = 100;
     public static final int LBL_WIDTH_BIG = 400;
     public static final int GAP_X = 5;
-    public static final int RESERVED_COLUMN_WIDTH = Math.max(GAP_X + SL_WIDTH + GAP_X + LBL_WIDTH, LBL_WIDTH_BIG);
+    public static final int RESERVED_HEIGHT_BOTTOM = 400;
     
     /** Creates a new Elastic Tree GUI for a k=6 fat tree */
     public ElasticTreeManager() {
@@ -49,35 +49,12 @@ public class ElasticTreeManager extends PZLayoutManager {
         dialPower.setPointerLine(1, 0.95);
         
         int max_gbps = 2 * fatTreeLayout.size_links(); /* 1Gbps in each direction per link */
-        dialBandwidth = new MultiPointerDial("Aggregate Xput", "Gbps", 2,  max_gbps, max_gbps/10);
+        dialBandwidth = new MultiPointerDial("Aggregate Throughput", "Gbps", 2,  max_gbps, max_gbps/10);
         dialBandwidth.setPointerLine(1, 0.95);
         
         dialLatency = new MultiPointerDial("Layer Latency", "msec", 3, 100, 10);
         
-        pnlSidebar.setDoubleBuffered(true);
-        pnlSidebar.setLayout(null);
-        pnlSidebar.add(slDemand);
-        pnlSidebar.add(lblDemand);
-        pnlSidebar.add(lblDemandVal);
-        pnlSidebar.add(slEdge);
-        pnlSidebar.add(lblEdge);
-        pnlSidebar.add(lblEdgeVal);
-        pnlSidebar.add(slAgg);
-        pnlSidebar.add(lblAgg);
-        pnlSidebar.add(lblAggVal);
-        pnlSidebar.add(slPLen);
-        pnlSidebar.add(lblPLen);
-        pnlSidebar.add(lblPLenVal);
-        pnlSidebar.add(lblTrafficMatrixCurrent);
-        pnlSidebar.add(lblTrafficMatrixNext);
-        pnlSidebar.add(lblResultInfo);
-        lblResultInfo.setVisible(false);
-        pnlSidebar.add(dialPower);
-        pnlSidebar.add(dialBandwidth);
-        pnlSidebar.add(dialLatency);
-        pnlSidebar.add(pnlAnim);
-        pnlSidebar.add(pnlMode);
-        initComponents();
+        initSidebarPanel();
     }
     
     // -------- Layout and Redrawing -------- //
@@ -125,12 +102,12 @@ public class ElasticTreeManager extends PZLayoutManager {
     public void attachWindow(final PZWindow w) {
         super.attachWindow(w);
         if(getWindowIndex(w) == 0) {
-            w.getContentPane().add(pnlSidebar);
-            w.setReservedWidthRight(RESERVED_COLUMN_WIDTH);
+            w.getContentPane().add(pnlCustom);
+            w.setReservedHeightBottom(RESERVED_HEIGHT_BOTTOM);
             w.setMySize(w.getWidth(), w.getHeight(), w.getZoom());
         }
     }
-    
+
     /**
      * Overrides parent to reduce size of node layout to provide room for other
      * widgets on the GUI.  Also lays out the Elastic Tree specific widgets. 
@@ -138,66 +115,17 @@ public class ElasticTreeManager extends PZLayoutManager {
     public void setLayoutSize(int w, int h) {
         super.setLayoutSize(w, h);
         relayout();
-
-        // place our custom components
-        pnlSidebar.setBounds(w, 0, RESERVED_COLUMN_WIDTH-4, h);
         
-        int x = GAP_X;
-        int y = 0;
-        lblTrafficMatrixCurrent.setBounds(x, y, LBL_WIDTH_BIG, LBL_HEIGHT);
-        y += LBL_HEIGHT;
-        lblTrafficMatrixNext.setBounds(x, y, LBL_WIDTH_BIG, LBL_HEIGHT);
-        y += LBL_HEIGHT;
-        lblResultInfo.setBounds(x, y, LBL_WIDTH_BIG, LBL_HEIGHT);
+        // choose a reasonable size for the dials based on the available width
+        int margin = 20;
+        int sz = Math.min(RESERVED_HEIGHT_BOTTOM, w/4) - 3 * margin;
+        Dimension prefDialSize = new Dimension(sz, sz);
+        dialPower.setPreferredSize(prefDialSize);
+        dialBandwidth.setPreferredSize(prefDialSize);
+        dialLatency.setPreferredSize(prefDialSize);
         
-        h -= y;
-        int SL_HEIGHT = h / 8;
-        final int SL_X = GAP_X;
-        
-        x = GAP_X;
-        y = h - SL_HEIGHT - LBL_HEIGHT;
-        final int LOWER_SL_HEIGHT = SL_HEIGHT - LBL_HEIGHT;
-        int LBL_Y = y + LOWER_SL_HEIGHT;
-        slDemand.setBounds(x, y, SL_WIDTH, LOWER_SL_HEIGHT);
-        lblDemand.setBounds(x, LBL_Y, LBL_WIDTH, LBL_HEIGHT);
-        lblDemandVal.setBounds(x, LBL_Y+LBL_HEIGHT, LBL_WIDTH, LBL_HEIGHT);
-        
-        int x2 = x + SL_WIDTH + GAP_X;
-        slPLen.setBounds(x2, y, LBL_WIDTH, LOWER_SL_HEIGHT);
-        lblPLen.setBounds(x2, LBL_Y, LBL_WIDTH, LBL_HEIGHT);
-        lblPLenVal.setBounds(x2, LBL_Y+LBL_HEIGHT, LBL_WIDTH, LBL_HEIGHT);
-        
-        x = SL_X;
-        y = h / 3 - 2 * SL_HEIGHT / 3;
-        LBL_Y = y + SL_HEIGHT;
-        slAgg.setBounds(x, y, SL_WIDTH, SL_HEIGHT);
-        lblAgg.setBounds(x, LBL_Y, LBL_WIDTH, LBL_HEIGHT);
-        lblAggVal.setBounds(x, LBL_Y+LBL_HEIGHT, LBL_WIDTH, LBL_HEIGHT);
-        
-        y = 2 * h / 3 - SL_HEIGHT;
-        LBL_Y = y + SL_HEIGHT;
-        slEdge.setBounds(x, y, SL_WIDTH, SL_HEIGHT);
-        lblEdge.setBounds(x, LBL_Y, LBL_WIDTH, LBL_HEIGHT);
-        lblEdgeVal.setBounds(x, LBL_Y+LBL_HEIGHT, LBL_WIDTH, LBL_HEIGHT);
-        
-        int o = SL_WIDTH + GAP_X;
-        int sz = LBL_WIDTH_BIG - o - GAP_X * 2;
-        x = x + o;
-        y = 3 * LBL_HEIGHT + 5;
-        int y_margin = 10;
-        if((sz+y_margin) * 3 + y > h)
-            sz = (h - y) / 3 - y_margin;
-        
-        dialPower.setBounds(x, y, sz, sz);
-        y += sz + y_margin;
-        dialBandwidth.setBounds(x, y, sz, sz);
-        y += sz + y_margin;
-        dialLatency.setBounds(x, y, sz, sz);
-        
-        pnlAnim.setLocation(0, 0);
-        pnlAnim.setSize(pnlAnim.getPreferredSize());
-        pnlMode.setLocation(0, 500);
-        pnlMode.setSize(pnlMode.getPreferredSize());
+        // relayout the custom part of the GUI 
+        pnlCustom.setBounds(0, h + margin, w, RESERVED_HEIGHT_BOTTOM - 2 * margin);
     }
     
     /**
@@ -281,27 +209,22 @@ public class ElasticTreeManager extends PZLayoutManager {
         }
     }
     
-    private JPanel pnlSidebar = new JPanel();
-    
-    private JLabel lblDemand = new JLabel("Demand", SwingConstants.LEFT);
-    private JLabel lblEdge = new JLabel("Edge", SwingConstants.LEFT);
-    private JLabel lblAgg = new JLabel("Agg", SwingConstants.LEFT);
-    private JLabel lblPLen = new JLabel("PktLen", SwingConstants.CENTER);
-
-    private JLabel lblDemandVal = new JLabel(StringOps.formatBitsPerSec(1000*1000*1000), SwingConstants.LEFT);
-    private JLabel lblEdgeVal = new JLabel("100%", SwingConstants.LEFT);
-    private JLabel lblAggVal = new JLabel("100%", SwingConstants.LEFT);
-    private JLabel lblPLenVal = new JLabel("1514B", SwingConstants.CENTER);
-
-    private JSlider slDemand = new MyJSlider(SwingConstants.VERTICAL, 0, 1000*1000*1000, 1000*1000*1000);
-    private JSlider slEdge   = new MyJSlider(SwingConstants.VERTICAL, 0, 100, 100);
-    private JSlider slAgg    = new MyJSlider(SwingConstants.VERTICAL, 0, 100, 100);
-    private JSlider slPLen    = new MyJSlider(SwingConstants.VERTICAL, 64, 1514, 1514);
-
+    private JPanel pnlCustom = new JPanel();
+    private final MultiPointerDial dialPower, dialBandwidth, dialLatency;
     private JLabel lblTrafficMatrixCurrent = new JLabel();
     private JLabel lblTrafficMatrixNext = new JLabel();
     private JLabel lblResultInfo = new JLabel();
     
+    private JPanel pnlTraffic = new JPanel();
+    private JPanel pnlDemand = new JPanel();
+    private JSlider slDemand = new MyJSlider(SwingConstants.HORIZONTAL, 0, 1000*1000*1000, 1000*1000*1000);
+    private JPanel pnlEdge = new JPanel();
+    private JSlider slEdge   = new MyJSlider(SwingConstants.HORIZONTAL, 0, 100, 100);
+    private JPanel pnlAgg = new JPanel();
+    private JSlider slAgg    = new MyJSlider(SwingConstants.HORIZONTAL, 0, 100, 100);
+    private JPanel pnlPLen = new JPanel();
+    private JSlider slPLen    = new MyJSlider(SwingConstants.HORIZONTAL, 64, 1514, 1514);
+
     private JPanel pnlAnim = new JPanel();
     private ButtonGroup optgrpAnim = new ButtonGroup();
     private JRadioButton optAnimNone = new JRadioButton("None");
@@ -318,6 +241,7 @@ public class ElasticTreeManager extends PZLayoutManager {
     private JComboBox cboModeK = new JComboBox(new String[] {"4", "6"});
     
     private static final Font TITLE_BORDER_FONT = new java.awt.Font("Tahoma", Font.BOLD, 16);
+    private static final Font TITLE_BORDER_FONT_SMALL = new java.awt.Font("Tahoma", Font.BOLD, 12);
     private static final Color TITLE_BORDER_FONT_COLOR = new java.awt.Color(0, 0, 128);
     
     private GroupLayout initGroupLayout(Container c) {
@@ -328,27 +252,106 @@ public class ElasticTreeManager extends PZLayoutManager {
         return layout;
     }
     
+    private void setPanelTitle(JPanel pnl, String title, Font f) {
+        pnl.setBorder(BorderFactory.createTitledBorder(null, 
+                title, 
+                TitledBorder.DEFAULT_JUSTIFICATION, 
+                TitledBorder.DEFAULT_POSITION, 
+                f,
+                TITLE_BORDER_FONT_COLOR));
+    }
+    
     private GroupLayout initPanel(JPanel pnl, String title) {
         return initPanel(pnl, title, TITLE_BORDER_FONT);
     }
     
     private GroupLayout initPanel(JPanel pnl, String title, Font f) {
-        pnl.setBorder(BorderFactory.createTitledBorder(null, 
-                                                       title, 
-                                                       TitledBorder.DEFAULT_JUSTIFICATION, 
-                                                       TitledBorder.DEFAULT_POSITION, 
-                                                       f,
-                                                       TITLE_BORDER_FONT_COLOR));
+        setPanelTitle(pnl, title, f);
         
         return initGroupLayout(pnl);
     }
     
-    /** layout and initialize all panels */
-    private void initComponents() {
+    /** layout and initialize the sidebar panel and its components */
+    private void initSidebarPanel() {
+        GroupLayout layout = initPanel(pnlCustom, "");
+        pnlCustom.setBorder(null);
+        
+        layout.setHorizontalGroup(
+                layout.createSequentialGroup()
+                    .addComponent(dialPower)
+                    .addComponent(dialBandwidth)
+                    .addComponent(dialLatency)
+                    .addGroup(layout.createParallelGroup()
+                        .addComponent(pnlTraffic))
+                    .addGroup(layout.createParallelGroup()
+                        .addComponent(pnlAnim)
+                        .addComponent(pnlMode)
+                        .addComponent(lblTrafficMatrixCurrent)
+                        .addComponent(lblTrafficMatrixNext)
+                        .addComponent(lblResultInfo))
+        );
+        
+        layout.setVerticalGroup(
+                layout.createParallelGroup()
+                        .addComponent(dialPower)
+                        .addComponent(dialBandwidth)
+                        .addComponent(dialLatency)
+                    .addGroup(layout.createSequentialGroup()
+                        .addComponent(pnlTraffic))
+                    .addGroup(layout.createSequentialGroup()
+                        .addComponent(pnlAnim)
+                        .addComponent(pnlMode)
+                        .addComponent(lblTrafficMatrixCurrent)
+                        .addComponent(lblTrafficMatrixNext)
+                        .addComponent(lblResultInfo))
+        );
+        
+        lblResultInfo.setVisible(false);        
+        
+        layout.linkSize(SwingConstants.HORIZONTAL, dialPower, dialBandwidth, dialLatency);
+        layout.linkSize(SwingConstants.VERTICAL, dialPower, dialBandwidth, dialLatency);
+        layout.linkSize(SwingConstants.VERTICAL, lblTrafficMatrixCurrent, lblTrafficMatrixNext, lblResultInfo);
+        layout.linkSize(SwingConstants.HORIZONTAL, pnlTraffic, pnlAnim, pnlMode);
+        
+        initTrafficPanel();
         initAnimPanel();
         initModePanel();
     }
 
+    /** layout and initialize the traffic panel and its components */
+    private void initTrafficPanel() {
+        GroupLayout layout = initPanel(pnlTraffic, "Traffic Control");
+        
+        setPanelTitle(pnlDemand, "Demand",            TITLE_BORDER_FONT_SMALL);
+        setPanelTitle(pnlAgg,    "Aggregation Layer", TITLE_BORDER_FONT_SMALL);
+        setPanelTitle(pnlEdge,   "Edge Layer",        TITLE_BORDER_FONT_SMALL);
+        setPanelTitle(pnlPLen,   "Packet Length",     TITLE_BORDER_FONT_SMALL);
+        
+        pnlDemand.add(slDemand);
+        pnlAgg.add(slAgg);
+        pnlEdge.add(slEdge);
+        pnlPLen.add(slPLen);
+        
+        layout.setHorizontalGroup(
+                layout.createParallelGroup()
+                    .addComponent(pnlDemand)
+                    .addComponent(pnlAgg)
+                    .addComponent(pnlEdge)
+                    .addComponent(pnlPLen)
+        );
+        
+        layout.setVerticalGroup(
+                layout.createSequentialGroup()
+                    .addComponent(pnlDemand)
+                    .addComponent(pnlAgg)
+                    .addComponent(pnlEdge)
+                    .addComponent(pnlPLen)
+        );
+        
+        layout.linkSize(SwingConstants.HORIZONTAL, pnlDemand, pnlAgg, pnlEdge, pnlPLen);
+        layout.linkSize(SwingConstants.VERTICAL, pnlDemand, pnlAgg, pnlEdge, pnlPLen);
+    }
+    
     /** layout and initialize the animation panel and its components */
     private void initAnimPanel() {
         GroupLayout layout = initPanel(pnlAnim, "Animation");
@@ -498,8 +501,6 @@ public class ElasticTreeManager extends PZLayoutManager {
         lblTrafficMatrixNext.setText("Next Traffic: " + s);
     }
 
-    private final MultiPointerDial dialPower, dialBandwidth, dialLatency;    
-    
     public void setPowerData(int cur, int traditional, int max) {
         dialPower.setValue(0, cur);
         dialPower.setValue(1, traditional);
@@ -563,10 +564,10 @@ public class ElasticTreeManager extends PZLayoutManager {
      * Updates the slider labels and notify those listening for traffic matrix changes.
      */
     public void notifyTrafficMatrixChangeListeners() {
-        lblDemandVal.setText(StringOps.formatBitsPerSec(slDemand.getValue()));
-        lblEdgeVal.setText(slEdge.getValue() + "%");
-        lblAggVal.setText(slAgg.getValue() + "%");
-        lblPLenVal.setText(slPLen.getValue() + "B");
+        setPanelTitle(pnlDemand, "Demand: " + StringOps.formatBitsPerSec(slDemand.getValue()), TITLE_BORDER_FONT_SMALL);
+        setPanelTitle(pnlAgg,    "Aggregation Layer: " + slAgg.getValue() + "%", TITLE_BORDER_FONT_SMALL);
+        setPanelTitle(pnlEdge,   "Edge Layer: " + slEdge.getValue() + "%", TITLE_BORDER_FONT_SMALL);
+        setPanelTitle(pnlPLen,   "Packet Length: " + slPLen.getValue() + "B", TITLE_BORDER_FONT_SMALL);
         
         ETTrafficMatrix tm = getCurrentTrafficMatrix();
         for(TrafficMatrixChangeListener c : trafficMatrixChangeListeneres)

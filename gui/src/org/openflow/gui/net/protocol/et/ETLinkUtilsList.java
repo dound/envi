538f63db2385a7527990a92083c3892e381c536a
David Underhill <dgu@cs.stanford.edu>
2009-04-18 19:07:45 -0700

    remove unused import

--- gui/src/org/openflow/gui/net/protocol/et/ETLinkUtilsList.java
+++ gui/src/org/openflow/gui/net/protocol/et/ETLinkUtilsList.java
@@ -4,7 +4,6 @@ import java.io.DataInput;
 import java.io.DataOutput;
 import java.io.IOException;
 
-import org.openflow.gui.net.protocol.Node;
 import org.openflow.gui.net.protocol.OFGMessage;
 import org.openflow.gui.net.protocol.OFGMessageType;
 

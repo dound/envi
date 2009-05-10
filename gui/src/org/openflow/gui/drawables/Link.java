21c95c7c396e747106afbdeae60d7faf74c6d9f7
David Underhill <dgu@cs.stanford.edu>
2009-05-09 20:13:18 -0700

    enable extensions to easily override the color of completely unutilized links

--- gui/src/org/openflow/gui/drawables/Link.java
+++ gui/src/org/openflow/gui/drawables/Link.java
@@ -46,6 +46,9 @@ import org.pzgui.math.Vector2i;
  * @author David Underhill
  */
 public class Link extends AbstractDrawable implements Edge<NodeWithPorts> {
+    /** how to color the link when it is completely unutilized */
+    public static Color USAGE_COLOR_0 = Color.BLACK;
+
     /**
      * This exception is thrown if a link which already exists is tried to be 
      * re-created.
@@ -745,6 +748,8 @@ public class Link extends AbstractDrawable implements Edge<NodeWithPorts> {
         if(usage < 0) {
             return Color.BLUE; // indicates that we don't know the utilization
         }
+        else if(usage == 0)
+            return USAGE_COLOR_0;
         else if (usage > 1)
             usage = 1;
         
@@ -756,7 +761,7 @@ public class Link extends AbstractDrawable implements Edge<NodeWithPorts> {
      */
     private static Color computeUsageColor(float usage) {
         if(usage < 0.0f)
-            return new Color(0.3f, 0.3f, 0.3f, 0.5f); // faded gray
+            return Color.BLUE;
         else {
             float mid = 1.5f / 3.0f;
             

35cac026e0d895ca5f353cab593a3c77afd1ff0d
Glen Gibb <grg@stanford.edu>
2009-08-02 13:58:04 -0700

    Renamed isPreparing to isReady

--- gui/src/org/openflow/gui/net/protocol/op/OPModule.java
+++ gui/src/org/openflow/gui/net/protocol/op/OPModule.java
@@ -34,9 +34,9 @@ public class OPModule extends Node {
         return (clid << 32L) | mid;
     }
     
-    /** check if the ID indicates that the node is being prepared */
-    public static final boolean isPreparing(long id) {
-        return (0x8000000000000000L & id) != 0;
+    /** check if the ID indicates that the node is being ready */
+    public static final boolean isReady(long id) {
+        return (0x8000000000000000L & id) == 0;
     }
 
     /** name of the module */

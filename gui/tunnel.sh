83330c565c8c737e65ff8bb9620ae47ff99b6f04
Rob Sherwood <rob.sherwood@stanford.edu>
2009-08-09 15:46:20 -0700

    updated tunnel order

--- gui/tunnel.sh
+++ gui/tunnel.sh
@@ -3,9 +3,9 @@
 ssh \
 -L 2501:openflow3.stanford.edu:2503 \
 -L 2502:openflow6.stanford.edu:2503 \
--L 2503:openflow5.stanford.edu:2505 \
+-L 2507:openflow5.stanford.edu:2504 \
 -L 2504:openflow4.stanford.edu:2503 \
 -L 2505:openflow5.stanford.edu:2503 \
 -L 2506:hpn8.stanford.edu:2503 \
--L 2507:openflow5.stanford.edu:2504 \
+-L 2507:openflow5.stanford.edu:2505 \
 yuba.stanford.edu sleep 4294967295

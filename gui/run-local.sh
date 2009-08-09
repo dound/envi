0ee8e70d5aadce2cce47b35807c655f2e0b14ce1
Rob Sherwood <rob.sherwood@stanford.edu>
2009-08-09 14:03:26 -0700

    added file to run via tunnel

---
+++ gui/run-local.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+
+java -jar bin/openflow_gui.jar  \
+	localhost:2501 \
+	localhost:2502 \
+	localhost:2503 \
+	localhost:2504 \
+	localhost:2505 \
+	localhost:2506 \
+	localhost:2507 \
+
+

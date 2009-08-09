2814b5f202164ec8464613128fc76045274c6672
Rob Sherwood <rob.sherwood@stanford.edu>
2009-08-09 16:02:17 -0700

    re-fixed tunnel order

--- gui/tunnel.sh
+++ gui/tunnel.sh
@@ -3,7 +3,7 @@
 ssh \
 -L 2501:openflow3.stanford.edu:2503 \
 -L 2502:openflow6.stanford.edu:2503 \
--L 2507:openflow5.stanford.edu:2504 \
+-L 2503:openflow5.stanford.edu:2504 \
 -L 2504:openflow4.stanford.edu:2503 \
 -L 2505:openflow5.stanford.edu:2503 \
 -L 2506:hpn8.stanford.edu:2503 \

28920999974e6366f5b8efdbf72d95dffef35bf1
Rob Sherwood <rob.sherwood@stanford.edu>
2009-08-11 16:09:19 -0700

    updated run scripts

--- gui/run.sh
+++ gui/run.sh
@@ -1,14 +1,5 @@
 #!/bin/sh
 
-java -jar bin/openflow_gui.jar  \
-	openflow4 \
- 	openflow3 \
-	openflow5:2504 \
-	hpn8 \
-	openflow5 \
-	openflow5:2505 \
-
-
-	#openflow6 \
+java -jar bin/openflow_gui.jar  
 
 

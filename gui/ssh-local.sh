841b6820447d85edf956b340adad87f18447481c
Rob Sherwood <rob.sherwood@stanford.edu>
2009-08-12 23:50:29 -0700

    removed redundant script
    tunnel.sh already exists

--- gui/ssh-local.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-ssh -L 2501:openflow3.stanford.edu:2503 -L 2502:openflow6.stanford.edu:2503 -L 2507:openflow5.stanford.edu:2505 -L 2504:openflow4.stanford.edu:2503 -L 2505:openflow5.stanford.edu:2503 -L 2506:hpn8.stanford.edu:2503 yuba.stanford.edu
-
-

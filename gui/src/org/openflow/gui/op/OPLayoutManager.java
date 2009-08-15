91edff59312be8c8c62b52b3de2d0dd87c8d22ce
Glen Gibb <grg@stanford.edu>
2009-08-14 22:22:42 -0700

    Create a "minimal" view in layout manager
    In minimal view only a few select elements are shown.

--- gui/src/org/openflow/gui/op/OPLayoutManager.java
+++ gui/src/org/openflow/gui/op/OPLayoutManager.java
@@ -1,10 +1,14 @@
 package org.openflow.gui.op;
 
+import java.awt.Graphics2D;
+import java.util.Vector;
+
 import org.openflow.gui.drawables.OPModule;
 import org.openflow.gui.drawables.OPNodeWithNameAndPorts;
 import org.pzgui.Drawable;
 import org.pzgui.PZManager;
 import org.pzgui.PZWindow;
+import org.pzgui.math.Vector2i;
 
 /**
  * Provides OpenPipes-specific layout enhancements.
@@ -12,8 +16,15 @@ import org.pzgui.PZWindow;
  * @author David Underhill
  */
 public class OPLayoutManager extends PZManager {
-    public OPLayoutManager() {
+	/* Should we show everything? */
+	private boolean fullView;
+	
+    /** Which entities are always visible */
+    private Vector<Drawable> alwaysVisibleDrawables = new Vector<Drawable>();
+
+	public OPLayoutManager() {
         super(); 
+        fullView = true;
     }
     
     /**
@@ -79,4 +90,69 @@ public class OPLayoutManager extends PZManager {
             addWindow(0, 0, 1366, 768, 0, 0, 1.0f);
         super.run();
     }
+
+    @Override
+	public synchronized void redraw(PZWindow window) {
+    	if (fullView)
+    		super.redraw(window);
+    	else {
+	    	
+	        // get GUI fields which affect the drawing process
+	        Graphics2D gfx = window.getDisplayGfx();
+	        if(gfx == null)
+	            return;
+	        
+	        // setup the view based on the pan and zoom settings
+	        Vector2i offset = new Vector2i(window.getDrawOffsetX(), window.getDrawOffsetY());
+	        float zoom = window.getZoom();
+	        setupGraphicsView(gfx, offset, zoom);
+	        
+	        // note that all nodes are undrawn at this point
+	        for(Drawable e : alwaysVisibleDrawables)
+	            e.unsetDrawn();
+	        
+	        // note that all nodes are undrawn at this point
+	        for(Drawable e : alwaysVisibleDrawables)
+	            drawObject(gfx, e);
+	        
+	        // back to the original view
+	        resetGraphicsView(gfx, offset, zoom);
+    	}
+	}
+    
+    /**
+     * Add a new entity to always draw on the GUI.
+     * @param d  the entity to start drawing
+     */
+    public synchronized void addAlwaysVisibleDrawable(Drawable d) {
+        // only draw each entity once
+        if(alwaysVisibleDrawables.contains(d))
+            return;
+        
+        // add to the front since it must be before everything else in drawables
+        alwaysVisibleDrawables.insertElementAt(d, 0);
+    }
+
+    /**
+     * Stop always drawing the specified entity.
+     * @param d  the entity to stop drawing
+     */
+    public synchronized void removeAlwaysVisibleDrawable(Drawable d) {
+        alwaysVisibleDrawables.remove(d);
+    }
+    
+    /**
+     * Show the full view with all drawables
+     */
+    public void showFullView() {
+    	fullView = true;
+    }
+    
+    /**
+     * Show the minimal view with only always visible drawables
+     */
+    public void showMinimalView() {
+    	fullView = false;
+    }
+
 }

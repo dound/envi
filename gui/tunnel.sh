43b9393cf2fe7bad6fda359289c804ed11afa6d9
Rob Sherwood <rob.sherwood@stanford.edu>
2009-11-16 14:30:12 -0800

    fixed port on agregagtion

--- gui/tunnel.sh
+++ gui/tunnel.sh
@@ -14,7 +14,7 @@ ssh \
 -L 2503:openflow5.stanford.edu:2507 \
 -L 2504:openflow3.stanford.edu:2503 \
 -L 2505:openflow5.stanford.edu:2506 \
--L 2506:openflow6.stanford.edu:2502 \
+-L 2506:openflow6.stanford.edu:2503 \
 yuba.stanford.edu 'while `true` ; do date ; sleep 20 ; done '
 #-L 2507:openflow6.stanford.edu:2503 \
 #-L 2504:openflow4.stanford.edu:2503 \
